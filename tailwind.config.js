30b44de3f891118f99caa0e01a3a0921773d7078
leopico <linkoko.peceng@gmail.com>
2023-03-16 17:24:26 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      inset: {
+        17: "68px",
+        26: "104px",
+      },
+    },
+  },
+  plugins: [],
+};
